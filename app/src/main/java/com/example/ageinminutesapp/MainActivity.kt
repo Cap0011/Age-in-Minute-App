960fd688aee4b4f2e02299232aa076c4e148f60b
Cap0011 <5165511@naver.com>
2021-01-09 00:08:38 +0900

    2021/01/08 - Layout design

--- app/src/main/java/com/example/ageinminutesapp/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutesapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ageinminutesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener{
+            Toast.makeText(this, "Button works", Toast.LENGTH_LONG).show()
+        }
     }
 }
